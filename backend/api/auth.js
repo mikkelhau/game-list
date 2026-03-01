c87234ff4c7f7fe9c6ec4bac82a9c1c92ca56eb4
mikkelhau <mikkel.hau@hotmail.com>
2026-03-01 16:34:34 +0100

    auth

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -1,3 +1,5 @@
-import express from 'express';
-import bcrypt from 'bcrypt';
-import { supabase } from '../supabaseClient.js';
+import express from "express";
+import bcrypt from "bcrypt";
+import { supabase } from "../supabaseClient.js";
+
+const router = express.Router();
